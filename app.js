61c751988880869090fdfc501ff86d9affd11885
jjuarezcal <jotajuarez@tramasolutions.com>
2018-01-26 10:46:48 +0100

    TYPE CLASES

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
-"use strict";
-console.log("probando");
-console.log("nueva prueba 2");
+// Uso de Let y Const
+var nombre = "Ricardo Tapia";
+var edad = 23;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+};
+var batman = {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"]
+};
+// Convertir esta funcion a una funcion de flecha
+//function resultadoDoble( a, b ){
+//  return (a + b) * 2
+//}
+var resultadoDoble = function (a, b) { return (a + b) * 2; };
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger(nombre, poder, arma) {
+    if (arma === void 0) { arma = "Arco"; }
+    var mensaje;
+    if (poder) {
+        // mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+        mensaje = nombre + " tienen el poder de: " + poder + " y un arma " + arma;
+        console.log(mensaje);
+    }
+    else {
+        mensaje = nombre + " tiene un " + poder;
+    }
+    console.log(mensaje);
+}
+;
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+var Rectangulo = /** @class */ (function () {
+    function Rectangulo() {
+    }
+    Rectangulo.prototype.calcularArea = function () {
+        return this.base * this.altura;
+    };
+    return Rectangulo;
+}());
